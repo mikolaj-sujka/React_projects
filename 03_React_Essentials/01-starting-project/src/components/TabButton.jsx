5a3c17c7907f0c91ed22d135187d4c2dc7539a8e
Mikołaj Sujka <mikolaj.sujka@qodeca.com>
2025-03-05 11:29:09 +0100

    Finishing first proj.

--- 03_React_Essentials/01-starting-project/src/components/TabButton.jsx
+++ 03_React_Essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,12 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({
+  children,
+  onSelect,
+  isSelected,
+  ...props
+}) {
   return (
     <li>
-      <button className={isSelected ? "active" : ""} onClick={onSelect}>
+      <button className={isSelected ? "active" : ""} {...props}>
         {children}
       </button>
     </li>
