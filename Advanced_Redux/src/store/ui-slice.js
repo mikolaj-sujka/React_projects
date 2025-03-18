5452b457bb4853305992cb52415d7ae20037df45
mikolaj-sujka <mikolajsujka@gmail.com>
2025-03-18 12:19:45 +0100

    working on advanced redux.

---
+++ Advanced_Redux/src/store/ui-slice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { cartIsVisible: false };
+
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
