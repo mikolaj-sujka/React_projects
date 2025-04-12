fec21a8295ab9b80837d760d57c2047e18f69a72
mikolaj-sujka <mikolajsujka@gmail.com>
2025-04-12 12:46:51 +0200

    Learning nextjs

---
+++ Next_js/foodies_app/app/components/main-header/nav-link.js
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import classes from "./nav-link.module.css";
+import { usePathname } from "next/navigation";
+
+export default function NavLink({ href, children }) {
+  const path = usePathname();
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={
+          path.startsWith(href)
+            ? `${classes.link} ${classes.active}`
+            : classes.link
+        }
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
---
+++ Next_js/foodies_app/app/meals/[mealSlug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDetailPage({ params }) {
+  return <h1>Meal Details</h1>;
+}
