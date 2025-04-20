3b0f53184f44c8bc89cbefd19d50366c2eccfe02
mikolaj-sujka <mikolajsujka@gmail.com>
2025-04-20 18:42:23 +0200

    Working on db & meals page.

--- Next_js/foodies_app/app/meals/page.js
+++ Next_js/foodies_app/app/meals/page.js
@@ -1,10 +1,22 @@
-
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created
+          <span className={classes.highlight}> by you</span>
+        </h1>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
