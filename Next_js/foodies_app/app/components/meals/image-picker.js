cbd3e822c04c047cb716977c457caec3a03f3aa7
mikolaj-sujka <mikolajsujka@gmail.com>
2025-04-23 20:21:53 +0200

    working on proj

---
+++ Next_js/foodies_app/app/components/meals/image-picker.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+
+  function handlePickClick(event) {
+    imageInput.current.click();
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          name={name}
+          accept="image/png, image/jpeg"
+          required
+          ref={imageInput}
+        />
+        <button
+          type="button"
+          className={classes.button}
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
